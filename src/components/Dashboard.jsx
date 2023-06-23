04da539093e724908d761a7226956a6c72eae143
MilagroRuizP <126681145+MilagroRuizP@users.noreply.github.com>
2023-06-23 12:04:31 -0400

    Add files via upload

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1 style={{ color: 'blue', textDecoration: 'underline' }}>Dashboard</h1>
+      
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h1 style={{ color: 'blue', textDecoration: 'underline' }}>Header</h1>
+      
+    </div>
+  );
+};
+
+export default Header;
